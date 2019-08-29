7891eade9db3a25ae1ca838d7a0f1c08a07954e9
Kevin Qu <kevinqu88@gmail.com>
2019-08-28 21:02:00 -0400

    heroku setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/chronometer'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/chronometer/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
+
+
