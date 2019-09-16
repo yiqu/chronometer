b518f0bfa90b02e8500d676608eb8077f46df767
Kevin Qu <kevinqu88@gmail.com>
2019-09-15 22:58:15 -0400

    eh

--- src/app/shared/table/table.module.ts
+++ src/app/shared/table/table.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { TableComponent } from './table.component';
+import { MaterialModule } from '../material-barrel.module';
 
 @NgModule({
   imports: [
+    MaterialModule
   ],
 
   exports: [
